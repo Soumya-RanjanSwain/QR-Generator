80360d4e6e4f62f80da7feaaea936c4e750bbb2e
Soumya Ranjan Swain <soumyaranjanswain063@gmail.com>
2023-07-01 15:06:00 +0530

    Added top rated Movies in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,52 @@ const Home = () => {
         "/trending/movie/day?language=en-US"
     );
 
+    const { data: Theatres, loading: Theatresloading } = useFetchData(
+        "/movie/top_rated?language=en-US&page=1"
+    );
+
     return (
         <div className="container mx-auto px-11 mb-8 pt-[90px] ">
             <Hero popular={Trending?.results} />
             <MovieCategory />
 
+            <h2 className="text-white my-10 text-3xl ">Top Rated Movies</h2>
+            {Theatresloading ? (
+                <div className=" text-white">
+                    <Loader />
+                </div>
+            ) : (
+                <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 gap-12 mb-9 ">
+                    {console.log(Theatres?.results)}
+                    {Theatres?.results.slice(0, 10).map((movie) => (
+                        <Link
+                            to={`/movie/${movie.id}`}
+                            className=" hover:scale-110 transition-all cursor-pointer duration-300 "
+                            key={movie.id}
+                        >
+                            <div className="relative">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    className=" hover:border-white  hover:border-[5px] transition-all duration-200"
+                                ></img>
+                                <div className=" w-[50px] h-[50px] bg-white absolute bottom-5 font-bold right-5 text-black flex justify-center items-center shadow-2xl rounded-full opacity-80">
+                                    {movie.vote_average}{" "}
+                                </div>
+                            </div>
+                            <div className="mt-3">
+                                <p className="text-white">{movie.title}</p>
+
+                                <p className="line-clamp-2 text-gray-500 mt-2">
+                                    {movie.overview}
+                                </p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+            )}
+
             <h2 className="text-white my-10 text-3xl ">Trending Movies</h2>
-            {TrendingLoading ? (
+            {Theatresloading ? (
                 <div className=" text-white">
                     <Loader />
                 </div>
